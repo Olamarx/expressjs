cd918c4b787ac27408db552318b2fd6c8ec63c7e
Olaoluwa Owoeye <Olaoluwaowoeye@gmail.com>
2024-09-05 21:30:13 +0100

    Added more learning

--- server.js
+++ server.js
@@ -1,8 +1,49 @@
+const { log } = require("console");
 const express = require("express");
+const path = require("path");
+const port = process.env.port;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({message: 'Hello World'});
+// Setup static folder
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+// app.get("/about", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "about.html"));
+// });
+
+let posts = [
+  { id: 1, title: "Post One" },
+  { id: 2, title: "Post Two" },
+  { id: 3, title: "Post Three" },
+  { id: 4, title: "Post Four" },
+  { id: 5, title: "Post Five" },
+];
+
+// To get all posts
+app.get("/api/posts", (req, res) => {
+  // console.log(req.query);
+  const limit = parseInt(req.query.limit);
+
+  if (!isNaN(limit) && limit > 0) {
+    return res.status(200).json(posts.slice(0, limit));
+  }
+  res.status(200).json(posts);
 });
-app.listen(8000, () => console.log(`Server is running on port 8000`));
+
+// To get a single post
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res.status(404).json({ msg: `A post with id ${id} was not found` });
+  }
+  res.status(200).json(post);
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
