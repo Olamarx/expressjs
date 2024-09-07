baae8526351407ce02f030114092c1a05fa4c5d7
Olaoluwa Owoeye <Olaoluwaowoeye@gmail.com>
2024-09-08 00:42:31 +0100

    Frontend working with backend API

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 const output = document.querySelector("#output");
 const button = document.querySelector("#get-posts-btn");
-
+// const formBtn = document.querySelector("#dd-post-form");
+const form = document.querySelector("#add-post-form");
 // Get and show posts
 async function showPosts() {
   try {
@@ -21,5 +22,35 @@ async function showPosts() {
   }
 }
 
+// Submit new post
+const addPost = async (e) => {
+  console.log("Hello");
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const title = formData.get("title");
+
+  try {
+    const res = await fetch("http://localhost:8000/api/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title }),
+    });
+    if (!res.ok) {
+      throw new Error("Failed to submit post");
+    }
+
+    const newPost = await res.json();
+    const postEl = document.createElement("div");
+    postEl.textContent = newPost.title;
+    output.appendChild(postEl);
+    showPosts();
+  } catch (error) {
+    console.log("Error not posting: ", error);
+  }
+};
+
 // Event Listeners
 button.addEventListener("click", showPosts);
+form.addEventListener("submit", addPost);
